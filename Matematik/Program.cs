12d9fa52ac893e6551e36e9ba6a10b905ab56484
BSevgi <baris@BSEVGI.KAPAKLI.YEREL>
2021-01-16 23:42:07 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortISlem dortISlem = new DortISlem();
+            dortISlem.Topla(2, 3);
+        }
+    }
+}
